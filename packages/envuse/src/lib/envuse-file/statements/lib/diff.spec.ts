27c947ae8f5de2ca685edd80a13c94a91dbeed9a
Jonathan Delgado <hi@jon.soy>
2021-07-13 11:16:52 -0400

    feat: implement test diff

--- packages/envuse/src/lib/envuse-file/statements/lib/diff.spec.ts
+++ packages/envuse/src/lib/envuse-file/statements/lib/diff.spec.ts
@@ -1,29 +1,121 @@
 import { EnvuseFileParser } from "../../envuse-file-parser";
 import { diff } from "./diff";
-import { b } from "./toBuffer"
+import { b } from "./toBuffer";
 
 describe("diff", () => {
-
   it("should diff deleted", () => {
+    const block1 = EnvuseFileParser.parseToAst(
+      Buffer.concat([
+        b("foo=bar\n"),
+        b("aaa=bbb\n"),
+        b("ccc=ddd\n"),
+        b("hhh=iii\n"),
+      ])
+    );
 
-    const block1 = EnvuseFileParser.parseToAst(Buffer.concat([
-      b("foo=bar\n"),
-      b("aaa=bbb\n"),
-      b("ccc=ddd\n"),
-      b("hhh=iii\n"),
-    ]))
+    const block2 = EnvuseFileParser.parseToAst(
+      Buffer.concat([
+        b("foo=bar\n"),
+        b("aaa=bbb\n"),
+        b("eee=fff\n"),
+        b("hhh=jjj\n"),
+      ])
+    );
 
-    const block2 = EnvuseFileParser.parseToAst(Buffer.concat([
-      b("foo=bar\n"),
-      b("aaa=bbb\n"),
-      b("eee=fff\n"),
-      b("hhh=jjj\n"),
-    ]));
-
-    console.log(diff(block1, block2));
-
-
-  })
-
-})
-
+    expect(diff(block1, block2)).toMatchInlineSnapshot(`
+      Object {
+        "changes": Array [
+          Object {
+            "base": Object {
+              "$type": "Variable",
+              "children": undefined,
+              "end": 24,
+              "keyVariable": Object {
+                "$type": "VariableKey",
+                "children": undefined,
+                "end": 19,
+                "pos": 16,
+                "value": "ccc",
+              },
+              "pos": 16,
+              "valueVariable": Object {
+                "$type": "VariableValue",
+                "children": undefined,
+                "end": 24,
+                "pos": 20,
+                "value": "ddd",
+              },
+            },
+            "type": "deleted",
+          },
+          Object {
+            "base": Object {
+              "$type": "Variable",
+              "children": undefined,
+              "end": 32,
+              "keyVariable": Object {
+                "$type": "VariableKey",
+                "children": undefined,
+                "end": 27,
+                "pos": 24,
+                "value": "hhh",
+              },
+              "pos": 24,
+              "valueVariable": Object {
+                "$type": "VariableValue",
+                "children": undefined,
+                "end": 32,
+                "pos": 28,
+                "value": "iii",
+              },
+            },
+            "compare": Object {
+              "$type": "Variable",
+              "children": undefined,
+              "end": 32,
+              "keyVariable": Object {
+                "$type": "VariableKey",
+                "children": undefined,
+                "end": 27,
+                "pos": 24,
+                "value": "hhh",
+              },
+              "pos": 24,
+              "valueVariable": Object {
+                "$type": "VariableValue",
+                "children": undefined,
+                "end": 32,
+                "pos": 28,
+                "value": "jjj",
+              },
+            },
+            "type": "changed",
+          },
+          Object {
+            "compare": Object {
+              "$type": "Variable",
+              "children": undefined,
+              "end": 24,
+              "keyVariable": Object {
+                "$type": "VariableKey",
+                "children": undefined,
+                "end": 19,
+                "pos": 16,
+                "value": "eee",
+              },
+              "pos": 16,
+              "valueVariable": Object {
+                "$type": "VariableValue",
+                "children": undefined,
+                "end": 24,
+                "pos": 20,
+                "value": "fff",
+              },
+            },
+            "type": "append",
+          },
+        ],
+      }
+    `);
+  });
+});
