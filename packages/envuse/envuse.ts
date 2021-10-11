4196a1c7f5a79925b426b01af46aa86bf2286b4a
Jonathan Delgado <hi@jon.soy>
2021-10-11 15:17:40 -0300

    style(envuse): update name variable path

--- packages/envuse/envuse.ts
+++ packages/envuse/envuse.ts
@@ -3,41 +3,46 @@ import { DataSource, Option, Values } from "./data-source/data-source";
 import { BlockType } from "./data-source/statements/comps/Block";
 import { SymbolEqual } from "./data-source/statements/comps/SymbolEqual";
 import { Variable } from "./data-source/statements/comps/Variable";
-import { createTypeScriptDefinition, EnvuseDefinition } from "./global-envuse-loaded";
+import {
+  createTypeScriptDefinition,
+  EnvuseDefinition,
+} from "./global-envuse-loaded";
 
-const variableLoader: { [k: string]: any } = {}
+const variableLoader: { [k: string]: any } = {};
 
 // ensure filepath is a file or return null
-const f = (filepath?: string) => {
+const file = (filepath?: string) => {
   if (filepath && fs.existsSync(filepath) && fs.statSync(filepath).isFile()) {
     return filepath;
   }
   return null;
 };
 
-
 export const defaultFilepath =
-  f(process.env.ENVUSE_FILE_PATH) ?? f(`${process.cwd()}/.envuse`);
+  file(process.env.ENVUSE_FILE_PATH) ?? file(`${process.cwd()}/.envuse`);
 
 export const defaultLockFilepath =
-  f(process.env.ENVUSE_LOCK_FILE_PATH) ?? `${process.cwd()}/.envuse-lock`;
-
+  file(process.env.ENVUSE_LOCK_FILE_PATH) ??
+  file(`${process.cwd()}/.envuse-lock`);
 
 /**
  * Load file .envuse or file defined on process.env.ENVUSE_FILE_PATH and put
  * values in to process.env
- * 
+ *
  * Make a lock file with the sample file and definition of .envuse file.
- * 
+ *
  * @hidden
  */
-export const register = (opts?: { filepath?: string, lockFilepath?: string }) => {
+export const register = (opts?: {
+  filepath?: string;
+  lockFilepath?: string;
+}) => {
   const filepath = opts?.filepath ?? defaultFilepath;
   const lockFilepath = opts?.lockFilepath ?? defaultLockFilepath;
 
   if (filepath) {
     const r = DataSource.parseFile(filepath, process.env);
-    const { definitions, parsed, ast } = r
+    const { definitions, parsed, ast } = r;
 
     global.envuseDataSourceParsed = r;
 
@@ -49,14 +54,19 @@ export const register = (opts?: { filepath?: string, lockFilepath?: string }) =>
 
     // Check if not there is a lock file
     if (!fs.existsSync(lockFilepath)) {
-      const bodyLock: number[] = [...ast.body]
-      ast.elementList.map(el => {
+      const bodyLock: number[] = [...ast.body];
+      ast.elementList.map((el) => {
         if (el instanceof Variable) {
-          const pos = el.elementList.find(el => el instanceof SymbolEqual)?.pos ?? el.valueVariable.pos
-          bodyLock.fill(0, pos, el.valueVariable.end)
+          const pos =
+            el.elementList.find((el) => el instanceof SymbolEqual)?.pos ??
+            el.valueVariable.pos;
+          bodyLock.fill(0, pos, el.valueVariable.end);
         }
-      })
-      fs.writeFileSync(lockFilepath, Buffer.from(bodyLock.filter(e => e !== 0)));
+      });
+      fs.writeFileSync(
+        lockFilepath,
+        Buffer.from(bodyLock.filter((e) => e !== 0))
+      );
     }
 
     for (const key in parsed) {
@@ -69,18 +79,18 @@ export const register = (opts?: { filepath?: string, lockFilepath?: string }) =>
 
 export const parse = (option: Option, values?: Values) => {
   return DataSource.parse(option, values);
-}
+};
 
 export const parseFile = (filepath: string, values: Values) => {
   return DataSource.parseFile(filepath, values);
-}
+};
 
 export const createDataSource = (option: Option) => {
   return DataSource.createDataSource(option);
-}
+};
 
 export const stringify = (comp: BlockType) => {
   return DataSource.stringify(comp);
-}
+};
 
 export default variableLoader as EnvuseDefinition;
